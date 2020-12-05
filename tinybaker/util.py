2718a54ba5020b570f123dd89ef124a8d8b0b9ab
Evin Sellin <evinism@gmail.com>
2020-12-05 00:18:42 -0800

    remove unnecessary absractions

--- tinybaker/util.py
+++ tinybaker/util.py
@@ -1,13 +1,6 @@
 from .workarounds.annot import is_fileset
 
 
-def affected_files_for_transform(transform):
-    return set.union(
-        get_files_in_path_dict(transform.input_paths),
-        get_files_in_path_dict(transform.output_paths),
-    )
-
-
 def get_files_in_path_dict(pathdict):
     retval = set()
     for tag in pathdict:
