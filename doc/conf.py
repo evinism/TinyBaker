45574ce9ee1df51a59c8655e677737c4d8fff062
Evin Sellin <evinism@gmail.com>
2020-12-14 23:02:48 -0800

    bump to 0.2.5

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Evin Sellin"
 author = "Evin Sellin"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.4"
+release = "0.2.5"
 
 
 # -- General configuration ---------------------------------------------------
