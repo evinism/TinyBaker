6eae9a177057a84d69fa8a570f969e6655e3b6c9
Evin Sellin <evinism@gmail.com>
2020-12-17 18:06:52 -0800

    bump to 0.3.0

--- tinybaker/cli.py
+++ tinybaker/cli.py
@@ -28,7 +28,7 @@ def _build_parser(TF: TransformMeta):
             help="Path for output tag {}".format(tag),
         )
 
-    parser.add_argument("--version", action="version", version="%(prog)s 0.2.5")
+    parser.add_argument("--version", action="version", version="%(prog)s 0.3.0")
     parser.add_argument(
         "--overwrite",
         dest="__overwrite__",
